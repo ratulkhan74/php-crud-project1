b71135898b3bb367802196dd4bf6723514f11082
ratulkhan74 <ratul.khan503040@gmail.com>
2023-09-01 04:17:20 +0600

    Added comments in database con file

--- db.php
+++ db.php
@@ -3,16 +3,16 @@
 define('SITEURL', 'http://localhost/php-project/php-crud/');
 
 // Database Information
-define('LOCALHOST', 'localhost');
-define('USERNAME', 'root');
-define('PASSWORD', '');
-define('DBNAME', 'crud');
+define('LOCALHOST', 'localhost'); // Database host name
+define('USERNAME', 'root'); // Database user name
+define('PASSWORD', ''); // Database password
+define('DBNAME', 'crud'); // Database name
 
-// Connecting with DB
+// Connecting Database
 $dbc = new mysqli(LOCALHOST, USERNAME, PASSWORD, DBNAME);
 // $dbc = mysqli_connect(LOCALHOST, USERNAME, PASSWORD, DBNAME);
 
-// Cheking DB Connection
+// Cheking Database Connection
 if (!$dbc) {
     // Error message
     die(mysqli_error($dbc));
